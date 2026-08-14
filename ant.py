0540e35397027b02ac214ddc2625ea81bbe7a67b
inaniotakas <inaniotakas@gmail.com>
2026-08-14 09:45:18 +0200

    feat: add directional ant visualization

--- ant.py
+++ ant.py
@@ -4,7 +4,7 @@ import pygame
 class Ant:
     def __init__(self, x, y):
         self.position = pygame.Vector2(x, y)
-        self.direction = pygame.Vector2(1, 0).normalize()
+        self.direction = pygame.Vector2(-1, -1).normalize()
         self.speed = 100
 
     #Actualizar estado del objeto
@@ -12,9 +12,20 @@ class Ant:
         self.position += self.direction * self.speed * dt
 
     def draw(self, screen):
+        
+        head_position = self.position + self.direction * 6
+        adbomen_position = self.position - self.direction * 5
+
         pygame.draw.circle(
             screen,
             (220, 180, 80),
-            self.position,
-            10
+            adbomen_position,
+            6
+        )
+
+        pygame.draw.circle(
+            screen,
+            (220, 180, 80),
+            head_position,
+            4
         )
